83e79747af1281f1c0e84881538d85df6b510d51
Paula Rösler <paularoesler@icloud.com>
2021-05-27 16:50:21 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -21,35 +21,36 @@ export default function App() {
 
 	return (
 		<AppWrapper>
-			<Header className="App-header">
+			<Header className='App-header'>
 				<h1>VOCAB</h1>
 			</Header>
 			{/* <div>{error && <p>{error}</p>}</div>
 				<div>Hui: {languages}</div> */}
 			<InputFormVocab
-				placeholderTextEnglish="Enter your new english vobaulary right here and ..."
-				placeholderTextGerman="... die deutsche Übersetzung dazu bitte hier."
+				placeholderTextEnglish='Enter your new english vobaulary right here and ...'
+				placeholderTextGerman='... die deutsche Übersetzung dazu bitte hier.'
 				disabledDe={false} // wenn das engl input feld zwei Buchstaben bekommen hat, soll das hier nicht disabled sein ? 'true' : 'false'
 				disabledSubmit={false} // wenn in beiden feldern min 2 Buchstaben sind = false
-				submitValue="add to stack"
+				submitValue='add to stack'
 				onAddVoc={addVoc}
 			/>
 			<VocabSets vocs={vocs} handleDeleteVoc={deleteVoc} />
 		</AppWrapper>
 	)
-
+	//'X-Secret': 'c14c6d99f4db49b3bb7890ba02bace8c6bfba7cb03689bd29fbf284095583bb3',
+	// "Authorization": "Basic {{token}}" + btoa('user:pass')}
 	function getAllVocabulary(
 		url = 'https://api.pons.com/v1/dictionaries?language=en'
 	) {
-		const apiHeader = {
+		fetch(url, {
 			method: 'GET',
 			credentials: 'omit',
 			headers: {
-				'HTTP-header':
+				'Content-Type': 'application/json',
+				Authorization:
 					'X-Secret: c14c6d99f4db49b3bb7890ba02bace8c6bfba7cb03689bd29fbf284095583bb3',
 			},
-		}
-		fetch(url, { apiHeader })
+		})
 			.then((res) => {
 				if (!res.ok) {
 					throw Error('Ooops!')
